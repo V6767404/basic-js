98674fdb03d615176fd7fe9247522ee3daa68369
v6767404 <v6767404@gmail.com>
2023-04-18 01:11:04 +0300

    feat: add get-email-domainmac-address matrix-element-sum sort-by-height sum-digits

--- src/sort-by-height.js
+++ src/sort-by-height.js
@@ -11,9 +11,19 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  * The result should be [-1, 150, 160, 170, -1, -1, 180, 190]
  */
-function sortByHeight(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function sortByHeight(arr) {
+  const sortedArr = arr.filter((num) => num !== -1).sort((a, b) => a - b);
+  const outputArr = [];
+  let index = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === -1) {
+      outputArr.push(arr[i]);
+    } else {
+      outputArr.push(sortedArr[index]);
+      index++;
+    }
+  }
+  return outputArr;
 }
 
 module.exports = {
--- src/sum-digits.js
+++ src/sum-digits.js
@@ -12,10 +12,12 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For 91, the result should be 1 (9 + 1 = 10, 1 + 0 = 1)
  *
  */
-function getSumOfDigits(/* n */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getSumOfDigits(n) {
+  let nStr = n + '';
+  if (nStr.length === 1) return n;
+  return getSumOfDigits(nStr.split('').reduce((acc, curr) => acc + Number(curr), 0));
 }
+
 
 module.exports = {
   getSumOfDigits
